70c91c304c623973676bc23e49fcb499264fda9a
Gabriel Cezar Muller <81782103+gabrielmuller1@users.noreply.github.com>
2022-04-04 01:03:31 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { RepositoryList } from "./components/RepositoryList";
+
+import "../src/styles/global.scss";
+
+export function App() {
+  return <RepositoryList />;
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const ReactRefresWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 const isDevelopment = process.env.NODE_ENV !== "production";
 
 module.exports = {
   mode: isDevelopment ? "development" : "production",
   devtool: isDevelopment ? "eval-source-map" : "source-map",
-  entry: path.resolve(__dirname, "src", "index.jsx"),
+  entry: path.resolve(__dirname, "src", "index.tsx"),
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
-  resolve: { 
-    extensions: [".js", ".jsx"],
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
   },
   devServer: {
     static: path.resolve(__dirname, "public"),
     hot: true,
   },
   plugins: [
-    isDevelopment && new ReactRefreshWebpackPlugin(),
+    isDevelopment && new ReactRefresWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "public", "index.html"),
     }),
@@ -28,7 +28,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx$/,
+        test: /\.(j|t)sx$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
